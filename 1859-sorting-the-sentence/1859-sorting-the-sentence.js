0ad6b0a47c09b47610afad5b5c299331b9786abd
Jithin P <147740632+Jithinp96@users.noreply.github.com>
2024-11-30 18:04:28 +0530

    Time: 49 ms (71.16%), Space: 49 MB (43.80%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var sortSentence = function(s) {
+    let res=[]
+    let arr=s.split(' ')
+    for(let i=0;i<arr.length;i++){
+        let index=arr[i][arr[i].length-1]-1
+        res[index]=arr[i].substring(0,arr[i].length-1)
+    }
+    return res.join(' ')
+};
