133380d79ec6a3fedded743d556bfbc30bb529ec
Jithin P <147740632+Jithinp96@users.noreply.github.com>
2024-12-02 18:33:12 +0530

    Time: 0 ms (100.00%), Space: 49.6 MB (69.65%) - LeetHub

---
+++ 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} num
+ * @return {boolean}
+ */
+var digitCount = function(num) {
+    for (let i=0; i<num.length;i++) {
+        let count = 0
+        for(let j = 0; j<num.length; j++) {
+            if(num[j] == i) {
+                count ++
+            }
+        }
+        if (count != num[i]){
+            return false
+        }  
+    }
+    return true
+};
